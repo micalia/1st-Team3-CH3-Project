ed1dc871fa214a95e246545af17784ef452de9b2
sonsazang <62087798+SONSAZANG@users.noreply.github.com>
2025-02-18 22:37:21 +0900

    [eunsu-001] Add 인벤토리 UI 및 아이템 베이스 생성

---
+++ ThreeFPS/Source/ThreeFPS/Public/Item/ItemDatabase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ItemDatabase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THREEFPS_API UItemDatabase : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
