2e656e40dfe5a9e329a49c67e696d0176ebf69e7
seung <seung991216@naver.com>
2025-02-19 18:13:00 +0900

    [jsh-002] Add 입력 액션 및 플레이어 컨트롤러 추가
    게임 모드에 Default PlayerController를 BP_ThreeFPSPlayerController로 설정해둠

---
+++ ThreeFPS/Source/ThreeFPS/Game/ThreeFPSPlayerController.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ThreeFPSPlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+class UEnhancedInputLocalPlayerSubsystem;
+
+/**
+ * 
+ */
+UCLASS()
+class THREEFPS_API AThreeFPSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AThreeFPSPlayerController();
+
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,Category="Input")
+	UInputMappingContext* MappingContext;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* MoveAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* LookAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* CrouchAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* JumpAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* FireAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* SprintAction;
+};
