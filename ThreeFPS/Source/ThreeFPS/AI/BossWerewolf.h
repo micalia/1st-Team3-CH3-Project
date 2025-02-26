4913a089f43488b65f2a2e5de0a7751b2ec4ea94
seung <seung991216@naver.com>
2025-02-26 20:39:15 +0900

    [jsh-005] Update 캐릭터 발사 기능 업데이트.

--- ThreeFPS/Source/ThreeFPS/AI/BossWerewolf.h
+++ ThreeFPS/Source/ThreeFPS/AI/BossWerewolf.h
@@ -22,7 +22,7 @@ protected:
 public:	
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	
     void CalculateDistance();
 	TObjectPtr<AThreeFPSCharacter> GetTarget(); 
 
