2191b2457c18168a43ee79a7c789f7453ff71455
seung <seung991216@naver.com>
2025-02-26 20:38:42 +0900

    [jsh-005] Add 동적 크로스헤어 추가.

---
+++ ThreeFPS/Source/ThreeFPS/Character/CrosshairWidget.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/CrosshairWidget.h"
+
+void UCrosshairWidget::UpdateCrosshairSize(const float Speed)
+{
+	if (!TopCrosshair || !BottomCrosshair || !LeftCrosshair || !RightCrosshair)
+		return;
+
+	// 위치 계산 (간격은 Speed, FireRate, Recoil 값을 기반으로 설정)
+	float Offset = FMath::Clamp(Speed * 0.1f, 0, 50.0f);
+
+	// 각 라인의 위치 조정
+	TopCrosshair->SetRenderTranslation(FVector2D(0, -Offset));
+	BottomCrosshair->SetRenderTranslation(FVector2D(0, Offset));
+	LeftCrosshair->SetRenderTranslation(FVector2D(-Offset, 0));
+	RightCrosshair->SetRenderTranslation(FVector2D(Offset, 0));
+}
---
+++ ThreeFPS/Source/ThreeFPS/Character/CrosshairWidget.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Image.h"
+#include "CrosshairWidget.generated.h"
+
+UCLASS()
+class THREEFPS_API UCrosshairWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+	//크로스헤어
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UImage* TopCrosshair;
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UImage* BottomCrosshair;
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UImage* LeftCrosshair;
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UImage* RightCrosshair;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Crosshair")
+	void UpdateCrosshairSize(const float Speed);
+};
